6279e9e715dde74227cc5a0a305ee76e22e8ddb0
shraddhasuruse <shraddha.suruse@vencentsystems.co.in>
2020-11-26 12:05:12 +0530

    unwanted file

--- backend/HM/Vegetable_Repository.java
+++
@@ -1,7 +0,0 @@
-package com.hotel_management.HM;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Vegetable_Repository extends JpaRepository<rawMaterial,Integer>{
-	rawMaterial findByName(String name);
-}
