b2b20bf4d36b1365cce3d4c1fa615e91642b89f5
Gopal Punvantwar <gopalpunvantwar@outlook.com>
2020-12-14 17:26:37 +0530

    CSS, Layout, Some screens are added.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { CustomerRegistrationformComponent } from './customer-registrationform/customer-registrationform.component';
 import { RawMaterialComponent } from './raw-material/raw-material.component';
 import { DepartmentComponent } from './department/department.component';
-import { RoleComponent } from './role/role.component';
+import { RoleComponent } from './pages/role/role.component';
+import { LayoutComponent } from './layout/layout.component';
+import { NavbarComponent } from './shared/component/navbar/navbar.component';
+import { FooterComponent } from './shared/component/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SettingService } from './shared/service/setting.service';
 
 @NgModule({
   declarations: [
@@ -16,14 +24,19 @@ import { RoleComponent } from './role/role.component';
     CustomerRegistrationformComponent,
     RawMaterialComponent,
     DepartmentComponent,
-    RoleComponent
+    RoleComponent,
+    LayoutComponent,
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
